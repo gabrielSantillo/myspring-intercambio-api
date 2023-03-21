ebb9e8954168825442355e1079ac902b8cf56940
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-03-21 17:15:18 -0600

    Starting MySpring Website API

---
+++ apihelpers.py
@@ -0,0 +1,20 @@
+import os
+from uuid import uuid4
+from dbhelpers import run_statement
+import smtplib
+import email.message
+
+# function responsible to the sent_data that will is going to be request.args or request.json and the
+# expected_data taht is going to be the list of keys the endpoint requires
+# this function will return a string in case of error and None otherwise
+def check_endpoint_info(sent_data, expected_data):
+    for data in expected_data:
+        if (sent_data.get(data) == None):
+            return f"The {data} argument is required."
+
+# function responsible fill in data was not sent by the client when trying to update data in the db
+def check_data_sent(sent_data, original_data, expected_data):
+    for data in expected_data:
+        if (sent_data.get(data) != None):
+            original_data[data] = sent_data[data]
+    return original_data
